fcb763442dea8cd5502eb3c3557c345613c15c95
ForkKILLET <Lx16302@163.com>
2019-08-28 11:31:45 +0800

    refactor: 改用 AJAX 从服务器获取 Markdown 内容。

--- main.js
+++ main.js
@@ -1,187 +1,69 @@
-let md_values =
-{
-	home:
-`
-# $fa fa-flag fa$ 这里是 IceLava 团队的站点。  
-蛤，你问 IceLava 是什么？  
-就是一个开发东西的团队啦！  
-我们目前刚刚搬家到这里（换域名），有些东西还没安置完毕……  
-
----
-
-不过我们的一些项目还是可以使用的呢！
-- $fali fa-gamepad fa$ [游戏：OI Nazo](OI-Nazo/index.html)  
-- $fali fa-gamepad fa$ [游戏：蛮荒大陆](-Barren-Land-/index.html)  
-- $fali fa-tag fa$ [工具：N $^ 2 ^$](N-2/index.html)  
-
----
-
-此外 IceLava 团队的成员 ForkΨKILLET 也会将他的博客搭建在这里\\~   
-其中有他的算法笔记、题解等内容，欢迎大佬指教！
-
----
-
-。  
-
-。  
-
-。  
-
-。  
-
-。  
-
-。  
-
-。  
-
-emmmm，有个事情忘了说，其实 IceLava 团队只有 ForkΨKILLET 一个人……  
-是不是很假…… QAQ
-
----
-
-_2019-08-23_
-`,
-	site_map:
-`
-# $fa fa-sitemap fa$ 这是本站的导航地图。
-_注：不是用于浏览器抓取的那种。_  
-
----
-
-$fali fa-home fa$ [主页：Mushouse 蘑菇树屋](https://icelava.ga/)
-- $fali fa-book fa$ 文章列表
-  - $fali fa-check fa$ [本页：站点地图 Site map](#)
-  - $fali fa-check fa$ [特殊：友链 Friends](https://icelava.ga/frineds)
-  - $fali fa-check fa$ [特殊：关于我们 About us](https://icelava.ga/about_us)
-  - $fali fa-rss-square fa$ [博客：ForkΨKILLET's](https://icelava.ga/blog_forkkillet/)
-    - $fali fa-warning fa$ _博客内容还未搬迁完毕，目前不予展示！_
-- $fali fa-gamepad fa$ [游戏：OI Nazo](https://icelava.ga/OI-Nazo/index.html)
-  - $fali fa-warning fa$ _因为本子站点是网址解密游戏，故其中页面不予展示 QAQ_
-- $fali fa-gamepad fa$ [游戏：Barren Land 蛮荒大陆](https://icelava.ga/-Barren-Land-/index.html)
-- $fali fa-tag fa$ [工具：N $^ 2 ^$](https://icelava.ga/N-2/index.html)
-
----
-
-_2019-08-23_
-`,
-	friends:
-`
-# $fa fa-heart fa$ 这里是 IceLava 团队的友链专区！
-想要互换友链我们 100% 欢迎！可以在 [About us 页面](http://icelava.ga/about_us) 找到联系我们的方式哦！  
-当然，您的网站必须要符合中华人民共和国法律法规才行呐~
-
----
-
-$fali fa-commenting fa$ [蒟扯官网](https://jc-juche.github.io/website/)：致力于专业扯淡的、富有亲和力的网站。  
-
----
-
-_2019-08-23_
-`,
-	about_us:
-`
-# $fa fa-info-circle fa$ 这里是关于 IceLava 的一些信息和联系方式等……
-本网站是由 IceLava 团队开发的，并且 IceLava 团队目前只有 ForkΨKILLET 一个人。
-本网站搭建于——  
-$fa fa-github fa$ [Github](http://github.com/ForkFG/ForkFG.github.io)，点击链接可前往本站的 Github Repo。  
-IceLava 团队在这里对 Github 表示衷心感谢，他们为我们提供了免费的静态页面储存库，使得我们的页面得以展示！
-
----
-
-本网站的域名来自 [freenom](https://freenom.com/)，虽然 ForkΨKILLET 出于某种原因废了好大一番周折才搞定……（笑）  
-IceLava 团队在这里也要感谢 freenom，我们敢说这是唯一一个有免费域名提供的网站了！没有他们，我们就不会有一个好记的域名。  
-并且，如果 IceLava 团队有了足够的“经费”，我们会第一时间在 freenom 换成付费域名！  
-此外我们还必须感谢 [CloudFlare](https://cloudflare.com) 提供的域名解析服务——真的非常快！
-
----
-
-本网站的计数器来自 [CuterCounter](https://www.cutercounter.com/)，不过我们没有听从警告，擅自去掉了计数器上的链接。  
-于是我们在这里补上链接，同时感谢 CuterCounter 为我们提供的免费网站计数器！
-
----
-
-本网站中各种图标都来自于 $fa fa-flag fa$ [Font Awesome](https://fontawesome.dashgame.com/)，这些图标都非常可爱，并且免费！  
-IceLava 团队在此感谢 Font Awesome，那里真的是一个有爱的地方 :)
-
----
-
-如果您愿意加入 IceLava 团队，或者希望与我们互换友链（见 [Friends 页面](https://icelava.ga/friends)），或者愿意在本站发表博客……  
-你可以用以下的方式联系我们：
-
-$fali fa-qq fa$ ForkKΨILLET 的 QQ：[1096694717](http://wpa.qq.com/msgrd?v=3&uin=1096694717&site=qq&menu=yes)  
-$fali fa-qq fa$ IceLava 的 QQ 群：[774225409](https://shang.qq.com/wpa/qunwpa?idkey=5177bf62ef83439d98cc73e2c0a12a75d9543b9a9c22b045c50babe1d2b4fb32)  
-$fali fa-envelope fa$ ForkΨKILLET 的邮箱： fork_killet@qq.com  
-$fali fa-envelope fa$ ForkΨKILLET 的另一个邮箱： lx16302@163.com  
-$fali fa-github fa$ ForkΨKILLET 的 Github 账号：[ForkFG](https://github.com/ForkFG/)  
-$fali fa-terminal fa$ ForkΨKILLET 的 洛谷 online judge 账号：[ForkΨKILLET](https://www.luogu.org/space/show?uid=125210)
-
----
-
-_2019-08-23_
-`,
-	blog_forkkillet_home:
-`
-# $fa fa-flag fa$ 这里有 ForkΨKILLET 的 blog 的文章列表。   
-数学公式测试：$math ax^2 + bx + c = \\text{qwq} math$  
-
-【正在施工】
-`,
-	sign_in:
-`
-# $fa fa-sign-in fa$ 在这里登录您的 Mushouse 账户！
-<form method='post' action='http://loli.icelava.ga/sign_in.php'>
-	<p class='text' style='text-align: center;'>
-		$fali fa-user-circle fa$ <input type='text' name='un' placeholder='Username' autocomplete='off'> <br>
-		$fali fa-lock fa$ <input type='password' name='pw' placeholder='Password' autocomplete='off'> <br>
-		<input type='submit' value='Submit'>
-	</p>
-</form>
-`
-}
-
 function log(info)
 {
-	console.log(info);
+	console.log(finfo);
 }
+
 function E(id)
 {
 	return document.getElementById(id);
 }
 
+function AJAX(type, url, MIME, value, fn_cb)
+{
+	let XHR = new XMLHttpRequest();
+	XHR.open(type, url);
+	XHR.setRequestHeader("Content-Type", MIME);
+	XHR.onreadystatechange = 
+	function()
+	{
+		if (XHR.readyState == 4 && XHR.status == 200)fn_cb(XHR);
+	}
+	XHR.send(value);
+}
+
 function init()
 {
 	E("guide").innerHTML =
-`
-<a href='https://icelava.ga/'><p id='home'>
-	<i class='fa fa-home'></i> Home
-</p></a>
-<a href='https://icelava.ga/site_map'><p class='text'>
-	<i class='li-icon fa-fw fa fa-sitemap'></i> Site Map
-</p></a>
-<a href='https://icelava.ga/friends'><p class='text'>
-	<i class='li-icon fa-fw fa fa-heart'></i> Friends
-</p></a>
-<a href='https://icelava.ga/about_us'><p class='text'>
-	<i class='li-icon fa-fw fa fa-info-circle'></i> About us
-</p></a> <hr>
+	`
+	<a href='https://icelava.ga/'><p id='home'>
+		<i class='fa fa-home'></i> Home
+	</p></a>
+	<a href='https://icelava.ga/site_map'><p class='text'>
+		<i class='li-icon fa-fw fa fa-sitemap'></i> Site Map
+	</p></a>
+	<a href='https://icelava.ga/friends'><p class='text'>
+		<i class='li-icon fa-fw fa fa-heart'></i> Friends
+	</p></a>
+	<a href='https://icelava.ga/about_us'><p class='text'>
+		<i class='li-icon fa-fw fa fa-info-circle'></i> About us
+	</p></a> <hr>
 
-<p class='text'><i class='li-icon fa-fw fa fa-rss-square'></i> Blogs</p>
-<a href='https://icelava.ga/blog_forkkillet'><p class='text'>
-	<i class='li-icon fa-fw fa fa-check'></i> ForkΨKILLET's
-</p></a> <hr>
+	<p class='text'><i class='li-icon fa-fw fa fa-rss-square'></i> Blogs</p>
+	<a href='https://icelava.ga/blog_forkkillet'><p class='text'>
+		<i class='li-icon fa-fw fa fa-check'></i> ForkΨKILLET's
+	</p></a> <hr>
 
-<img id='logo' src='https://icelava.ga/icelava.jpg' width='210px'>
-<p class='text'>
-	<span style='color: #ff5b5a'>RED</span> ICE - WE<br>
-	<span style='color: #0e61e1'>blue</span> lava - world
-</p> <hr>
+	<img id='logo' src='https://icelava.ga/icelava.jpg' width='210px'>
+	<p class='text'>
+		<span style='color: #ff5b5a'>RED</span> ICE - WE<br>
+		<span style='color: #0e61e1'>blue</span> lava - world
+	</p> <hr>
 
-<p class='text'>
-	<i class='li-icon fa-fw fa fa-mouse-pointer'></i> Hits
-	<img id='counter' src='https://www.cutercounter.com/hits.php?id=geqpdpp&nd=7&style=72'> 
-</p>
-`
+	<p class='text'>
+		<i class='li-icon fa-fw fa fa-mouse-pointer'></i> Hits
+		<img id='counter' src='https://www.cutercounter.com/hits.php?id=geqpdpp&nd=7&style=72'> 
+	</p>
+	`
+
+	MathJax.Hub.Config(
+	{
+		tex2jax:
+		{
+			inlineMath: [["$math ", " math$"]],
+			displayMath: [["$Math ", " Math$"]],
+			skipTags: ["script", "noscript", "style", "textarea", "pre", "code", "a"]
+		}
+	});
 
 	var MD = new marked.Renderer();
 	marked.setOptions(
@@ -222,21 +104,22 @@ function init()
 		return str;
 	}
 
+	let md_values = [];
 	let md_areas = document.getElementsByClassName("md");
 	let $i;
-	MathJax.Hub.Config(
-	{
-		tex2jax:
-		{
-			inlineMath: [["$math ", " math$"]],
-			displayMath: [["$Math ", " Math$"]],
-			skipTags: ["script", "noscript", "style", "textarea", "pre", "code", "a"]
-		}
-	});
 	for (let i = 0; i < md_areas.length; i++)
 	{
 		$i = md_areas[i];
-		$i.innerHTML = marked(md_values[$i.dataset.name]);
+
+		AJAX("GET", "http://loli.icelava.ga/load_md.php?name=" + $i.dataset.name, "application/x-www-form- urlencoded", null, 
+		function(XHR)
+		{
+			let $script = document.createElement("script");
+			document.head.appendChild($script);
+			$script.outerHTML = XHR.responseText;
+			$i.innerHTML = marked(md_values[$i.dataset.name].replace(/\t/g, ""));
+		});
+
 		MathJax.Hub.Queue(["Typeset", MathJax.Hub, $i]);
 	}
 }
