a42201cdf615d7c02de5f0e11ef2ac12797d0fd7
ForkKILLET <fork_killet@qq.com>
2019-09-14 10:05:14 +0800

    feat(blog/ForkKILLET, UI): 搞定了筛选文章的 UI。

--- main.js
+++ main.js
@@ -27,63 +27,67 @@ $(document).ready(function()
 {
     // Note: 插入导航栏（侧边栏）
     let $body = $("body");
-    $body.prepend("<div id='guide'></div>");
+    $body.prepend(`<div id="guide"></div>`);
     let $guide = $("#guide");
     $guide.html(
     `
-        <a href='http://icelava.ga/' class='title' id='home'>
-            <i class='fa fa-home'></i> Home
+        <a href="http://icelava.ga/" class="title" id="home">
+            <i class="fa fa-home"></i> Home
         </a>
         
-        <div id='user'>
-            <a class='text' href='http://icelava.ga/sign_up'>Sign up </a>
-            <p class='text'><i class='fa fa-fw fa-sign-in'></i></p>
-            <a class='text' href='http://icelava.ga/sign_in'>in</a>
+        <div id="user">
+            <a class="text" href="http://icelava.ga/sign_up">Sign up </a>
+            <p class="text"><i class="fa fa-fw fa-sign-in"></i></p>
+            <a class="text" target="_blank" href="http://icelava.ga/sign_in">in</a>
         </div> <hr>
         
-        <a href='http://icelava.ga/site_map' class='text'>
-            <i class='li-icon fa-fw fa fa-sitemap'></i> Site Map
+        <a href="http://icelava.ga/site_map" class="text">
+            <i class="li-icon fa-fw fa fa-sitemap"></i> Site Map
         </a>
         
-        <a href='http://icelava.ga/friends' class='text'>
-            <i class='li-icon fa-fw fa fa-heart'></i> Friends
+        <a href="http://icelava.ga/friends" class="text">
+            <i class="li-icon fa-fw fa fa-heart"></i> Friends
         </a>
         
-        <a href='http://icelava.ga/about_us' class='text'>
-            <i class='li-icon fa-fw fa fa-info-circle'></i> About us
+        <a href="http://icelava.ga/about_us" class="text">
+            <i class="li-icon fa-fw fa fa-info-circle"></i> About us
         </a>
         
-        <a href='http://icelava.ga/log' class='text'>
-            <i class='li-icon fa-fw fa fa-file-text'></i> Log
+        <a href="http://icelava.ga/log" class="text">
+            <i class="li-icon fa-fw fa fa-file-text"></i> Log
         </a> <hr>
     
-        <p class='text'><i class='li-icon fa-fw fa fa-rss-square'></i> Blogs</p>
-        <a href='http://icelava.ga/blogs/ForkKILLET' class='text'>
-            <i class='li-icon fa-fw fa fa-check'></i> ForkΨKILLET's
+        <p class="text"><i class="li-icon fa-fw fa fa-rss-square"></i> Blogs</p>
+        <a href="http://icelava.ga/blogs/ForkKILLET" class="text">
+            <i class="li-icon fa-fw fa fa-check"></i> ForkΨKILLET"s
         </a> <hr>
     
-        <img id='logo' alt='logo' src='http://icelava.ga/icelava.jpg' width='210px'>
-        <p class='text'>
-            <span style='color: #ff5b5a'>RED</span> ICE - WE<br>
-            <span style='color: #0e61e1'>blue</span> lava~world
+        <img id="logo" alt="logo" src="http://icelava.ga/icelava.jpg" width="210px">
+        <p class="text">
+            <span style="color: #ff5b5a">RED</span> ICE - WE<br>
+            <span style="color: #0e61e1">blue</span> lava~world
         </p> <hr>
     
-        <p class='text'>
-            <i class='li-icon fa-fw fa fa-mouse-pointer'></i> Hits
-            <img id='counter' alt='counter' src='http://www.cutercounter.com/hits.php?id=geqpdpp&nd=7&style=72'> 
+        <p class="text">
+            <i class="li-icon fa-fw fa fa-mouse-pointer"></i> Hits
+            <img id="counter" alt="counter" src="http://www.cutercounter.com/hits.php?id=geqpdpp&nd=7&style=72"> 
         </p>
     `);
-    $body.prepend("<div id='user_op'></div>");
+    $body.prepend(`<div id="user_op"></div>`);
 
     // Note: 用户面板（目前只有注销功能）
-    $("#user_op").hide().html("<p class='text'><i class='fa fa-sign-out'></i> 注销</p>");
+    $("#user_op").hide().html(`<p class="text"><i class="fa fa-sign-out"></i> 注销</p>`);
     $("#user_op>p").click(function()
     {
         AJAX("GET", "http://loli.icelava.ga/sign_out.php", "application/x-www-form-urlencoded", null,
-            function()
-            {
-                location.reload();
-            });
+        function()
+        {
+            location.reload();
+        });
+    });
+    $("#user:nth-child(3)").click(function()
+    {
+        location.reload();
     });
 
     let $user = $("#user");
@@ -99,11 +103,12 @@ $(document).ready(function()
     $pos.css("left", "calc(100% - 50px - " + $pos.width() + "px)");
     $pos.animate({"left": "300px"}, 1000, "swing");
 
-    // Done: 在 #main 下面写版权信息。
-    $body.append("<p id='copyright' class='text'>Copyright© 2019 IceLava Dev Team. All Rights Reserved</p>");
+    // Note: 在 #main 下面写版权信息。
+    $body.append(`<p id="copyright" class="text">Copyright© 2019 IceLava Dev Team. All Rights Reserved</p>`);
+    // Todo: 解决页面过窄时 Copyright 显示不当的问题。
 
     // Note: 如果不是本地，token 登录。
-    if (location.href[0] === "h")
+    if (location.href.indexOf("localhost") !== -1)
         AJAX("GET", "http://loli.icelava.ga/get_token.php", "application/x-www-form-urlencoded", null,
         function(XHR)
         {
@@ -112,7 +117,7 @@ $(document).ready(function()
             let un = JSON.parse(token)["un"];
             let $user = $("#user");
             $user.data("tourist", false);
-            $user.html("<p id='btn_user' class='text'><i class='li-icon fa-fw fa fa-user'></i> " + un + "</p>");
+            $user.html(`<p id="btn_user" class="text"><i class="li-icon fa-fw fa fa-user"></i> ` + un + `</p>`);
         });
 
     // Note: Tex 公式配置
@@ -151,9 +156,9 @@ $(document).ready(function()
         { from: /\$_/g, to: "<sub>" },
         { from: /_\$/g, to: "</sub>" },
 
-        { from: /\$fali/g, to: "<i class='li-icon fa fa-fw" },
-        { from: /\$fa/g, to: "<i class='fa " },
-        { from: /fa\$/g, to: "'></i>" },
+        { from: /\$fali/g, to: `<i class="li-icon fa fa-fw` },
+        { from: /\$fa/g, to: `<i class="fa ` },
+        { from: /fa\$/g, to: `"></i>` },
 
         { from: /\$dollar/g, to: "$" }
     ];
