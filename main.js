1542e73eea7aa201a5e14c39d86b656e165d49f4
ForkKILLET <fork_killet@qq.com>
2019-10-03 09:20:53 +0800

    refactor(UI): 重构样式！

--- main.js
+++ main.js
@@ -34,44 +34,24 @@ $(document).ready(function()
 	let $body = $("body");
 	$body.prepend(`<div id="guide"></div>`);
 	$("#guide").html([`
-        <a href="http://icelava.ga/" class="title" id="home">
-            <i class="fa fa-home"></i> Home
-        </a>
-        
+        <h1 id="home"><a href="http://icelava.ga/"><i class="fa fa-home"></i> Home</a></h1>
         <div id="user">
-            <a class="text" href="http://icelava.ga/sign_up">Sign up </a>
-            <p class="text"><i class="fa fa-sign-in"></i></p>
-            <a class="text" target="_blank" href="http://icelava.ga/sign_in">in</a>
+            <a href="http://icelava.ga/sign_up">Sign up </a>
+            <p><i class="fa fa-sign-in"></i></p>
+            <a target="_blank" href="http://icelava.ga/sign_in">in</a>
         </div> <hr>
-        
-        <a href="http://icelava.ga/site_map" class="text">
-            <i class="li-icon fa-fw fa fa-sitemap"></i> Site Map
-        </a>
-        
-        <a href="http://icelava.ga/friends" class="text">
-            <i class="li-icon fa-fw fa fa-heart"></i> Friends
-        </a>
-        
-        <a href="http://icelava.ga/about_us" class="text">
-            <i class="li-icon fa-fw fa fa-info-circle"></i> About us
-        </a>
-        
-        <a href="http://icelava.ga/log" class="text">
-            <i class="li-icon fa-fw fa fa-file-text"></i> Log
-        </a> <hr>
-    
-        <p class="text"><i class="li-icon fa-fw fa fa-rss-square"></i> Blogs</p>
-        <a href="http://icelava.ga/blogs/ForkKILLET" class="text">
-            <i class="li-icon fa-fw fa fa-check"></i> ForkΨKILLET's
-        </a> <hr>
-    
+        <a href="http://icelava.ga/site_map"><i class="li-icon fa-fw fa fa-sitemap"></i> Site Map</a>
+        <a href="http://icelava.ga/friends"><i class="li-icon fa-fw fa fa-heart"></i> Friends</a>
+        <a href="http://icelava.ga/about_us"><i class="li-icon fa-fw fa fa-info-circle"></i> About us</a>
+        <a href="http://icelava.ga/log"><i class="li-icon fa-fw fa fa-file-text"></i> Log</a> <hr>
+        <p><i class="li-icon fa-fw fa fa-rss-square"></i> Blogs</p>
+        <a href="http://icelava.ga/blogs/ForkKILLET"><i class="li-icon fa-fw fa fa-check"></i> ForkΨKILLET's</a> <hr>
         <img id="logo" alt="logo" src="https://s2.ax1x.com/2019/10/01/uNUSHg.md.jpg" width="210px">
-        <p class="text">
+        <p>
             <span style="color: #ff5b5a">RED</span> ICE - WE<br>
             <span style="color: #0e61e1">blue</span> lava~world
         </p> <hr>
-    
-        <p class="text">
+        <p>
             <i class="li-icon fa-fw fa fa-mouse-pointer"></i> Hits
             <img id="counter" alt="counter" src="http://www.cutercounter.com/hits.php?id=geqpdpp&nd=7&style=72"> 
         </p>
@@ -79,7 +59,7 @@ $(document).ready(function()
 	$body.prepend(`<div id="user_op"></div>`);
 
 	// Note: 用户功能
-	$("#user_op").hide().html(`<p class="text"><i class="fa fa-sign-out"></i> Sign out</p>`);
+	$("#user_op").hide().html(`<p><i class="fa fa-sign-out"></i> Sign out</p>`);
 	$("#user_op>.text:first-child").click(function()
 	{
 		AJAX("GET", "http://loli.icelava.ga/sign_out.php", "application/x-www-form-urlencoded", null,
@@ -107,7 +87,7 @@ $(document).ready(function()
 	    .animate({"left": "300px"}, 1000, "swing");
 
 	// Note: 在 #main 下面写版权信息。
-	$body.append(`<p id="copyright" class="text">Copyright© 2019 IceLava Dev Team. All Rights Reserved</p>`);
+	$body.append(`<p id="copyright">Copyright© 2019 IceLava Dev Team. All Rights Reserved</p>`);
 
 	// Note: 如果不是本地，token 登录。
 	if (!is_local())
@@ -119,7 +99,7 @@ $(document).ready(function()
 			let un = JSON.parse(token)["un"];
 			let $user = $("#user");
 			$user.data("tourist", false);
-			$user.html(`<p id="btn_user" class="text"><i class="li-icon fa-fw fa fa-user"></i> ` + un + `</p>`);
+			$user.html(`<p id="btn_user"><i class="li-icon fa-fw fa fa-user"></i> ` + un + `</p>`);
 		});
 
 	// Note: MD 渲染的一系列操作。
@@ -201,28 +181,28 @@ $(document).ready(function()
 					param:
 					[
 						{ // Note: 英文
-							begin: `<strong class="text">`,
+							begin: `<strong>`,
 							end: `$nbsp</strong>`,
 							time: 1
 						},
 						{ // Note: 音标
-							begin: `<p class="text">/`,
+							begin: `<p>/`,
 							end: `/$nbsp</p>`,
 							time: 1
 						},
 						{ // Note: 词性
-							begin: `<em class="text">`,
+							begin: `<em>`,
 							end: `$nbsp</em>`,
 							time: 1
 						},
 						{ // Note: 中文
-							begin: `<p class="text">`,
+							begin: `<p>`,
 							end: `$nbsp</p>`,
 							time: 1
 						},
 						{ // Note: 例句
-							begin: `<a class="text wordbox-stc-btn">@</a> <br> <div class="text wordbox-stc">`,
-							end: `</div>`,
+							begin: `<a>@</a> <br> <div><p>`,
+							end: `</p></div>`,
 							time: 1
 						}
 					]
@@ -232,14 +212,14 @@ $(document).ready(function()
 			{
 				wordbox: function(e)
 				{
-					$(e).find(".wordbox .wordbox-stc-btn:not(.ready)")
+					$(e).find(".wordbox>a:not(.ready)")
 						.click(function()
 						{
-							$(this).parent().children(".text:last-child").fadeToggle(500);
+							$(this).parent().children("*:last-child").fadeToggle(500);
 						})
 						.addClass("ready")
-						.parent().children(".text:last-child").hide();
-					let $p = $(e).find(".wordbox>.text:nth-child(2)");
+						.parent().children("*:last-child").hide();
+					let $p = $(e).find(".wordbox>p:nth-child(2)");
 					for (let i = 0; i < $p.length; i++)
 					{
 						let $i = $($p[i]);
