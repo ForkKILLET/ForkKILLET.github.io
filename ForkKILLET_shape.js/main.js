7e57d3b98873f85f9d7c97d3b6a77d694e4e0f80
ForkKILLET <fork_killet@qq.com>
2020-01-04 22:56:55 +0800

    BarrenLand/ [VER β0.3.0] feat(Module : CANVAS, MESSAGE): 添入覆盖整个游戏界面的画布，可用于绘制特效。预留了消息提示的位置。 feat(Animation : Transmit): 添入传送特效。 style: 移出 typeof 后的括号。 upd(Module : Info): 添入新年快落的文字。

--- ForkKILLET_shape.js/main.js
+++ ForkKILLET_shape.js/main.js
@@ -1,13 +1,48 @@
-Math.deg_to_rad = (deg) => deg * Math.PI / 180;
-Math.rad_to_deg = (rad) => rad / Math.PI * 180;
-Math.random_in_range = (min, max, if_int) =>
+if (!Object.prototype._am) Object.prototype._am = function (n, m)
+{
+	if (!this[n]) this[n] = m;
+	return this;
+};
+
+window
+._am("log", msg => console.log(msg))
+._am("time", (if_hms, date) =>
+{
+	let d = date ? date : new Date(),
+		Y = d.getFullYear(),
+		M = d.getMonth() + 1,
+		D = d.getDate(),
+		h = d.getHours(),
+		m = d.getMinutes(),
+		s = d.getSeconds();
+	if (M < 10) M = "0" + M;
+	if (D < 10) D = "0" + D;
+	if (h < 10) h = "0" + h;
+	if (m < 10) m = "0" + m;
+	if (s < 10) s = "0" + s;
+	return `${Y}-${M}-${D}` + (if_hms ? ` ${h}:${m}:${s}` : "");
+})
+._am("is_empty", n => n === null || n === undefined)
+._am("is_array", n => typeof n === "object" && n.constructor.name === "Array")
+._am("number_px", n => typeof n === "number" ? n + "px" : n);
+Math
+._am("deg_to_rad", (deg) => deg * Math.PI / 180)
+._am("rad_to_deg", (rad) => rad / Math.PI * 180)
+._am("random_in_range", (min, max, if_int) =>
 {
 	let r = Math.random() * (max - min + 1) + min;
 	if (if_int) r = Math.trunc(r);
 	return r;
-};
-Math.random_in_100 = () => Math.random_in_range(0, 100, true);
-if (!window.log) window.log = (msg) => console.log(msg);
+})
+._am("random_in_100", () => Math.random_in_range(0, 100, true));
+window
+._am("random_item", (arr) =>
+{
+	if (typeof arr === "object") return arr[Math.random_in_range(0, arr.length - 1, true)];
+	else return null;
+});
+
+delete Object.prototype._am;
 
 class ForkKILLETShape
 {
@@ -20,13 +55,30 @@ class ForkKILLETShape
 			throw e;
 		};
 
-		if (!$ || typeof($) !== "function") this.error_and_throw("R001", "Require JQuery but not found the correct $ function.");
+		if (!$ || typeof $ !== "function") this.error_and_throw("R001", "Require JQuery but not found the correct $ function.");
 		if (!window.script) window.script = {};
 		window.script.ForkKILLET_shape = true;
 		log("[LOAD]: icelava.top/ForkKILLET_shape.js/main.js");
+
+		this.default_id =
+		{
+			line: 0,
+			circle: 0,
+			text: 0
+		};
 	}
+
+	bind_canvas(id)
+	{
+		this.canvas = id;
+	}
+
 	line(pa, id, size, x1, y1, x2, y2, color)
 	{
+		if (is_empty(pa)) pa = this.canvas;
+		if (is_empty(id)) id = `FS_default_${this.default_id.line++}`;
+		size = number_px(size);
+
 		if (x2 < x1)
 		{
 			[x1, x2] = [x2, x1];
@@ -49,58 +101,59 @@ class ForkKILLETShape
 		$i.css("backgroundColor", color);
 		$i.css("transform", `rotate(${theta}deg)`);
 		$i.css("transformOrigin", "top left");
+
+		return $i;
 	}
 
 	circle(pa, id, s_size, x, y, r, s_color, i_color)
 	{
+		if (is_empty(pa)) pa = this.canvas;
+		if (is_empty(id)) id = `FS_default_${this.default_id.circle++}`;
+		if (typeof s_size === "number") s_size += "px";
+
 		$(`#${pa}`).append($(`<div id="circle_${id}" class="circle"></div>`));
 		let $i = $(`#circle_${id}`);
-		$i.css("top", `${y - r}px`);
-		$i.css("left", `${x - r}px`);
-		$i.width(r * 2);
-		$i.height(r * 2);
-		$i.css("borderWidth", `${s_size}px`);
-		$i.css("borderColor", s_color);
-		$i.css("backgroundColor", i_color);
+		$i
+		.css("top", `${y - r}px`)
+		.css("left", `${x - r}px`)
+		.width(r * 2).height(r * 2)
+		.css("borderWidth", s_size)
+		.css("borderColor", s_color)
+		.css("backgroundColor", i_color)
+		.hover(() => $i.css("borderColor", i_color).css("backgroundColor", s_color),
+		       () => $i.css("borderColor", s_color).css("backgroundColor", i_color));
 
-		// Note: 悬停动画
-		$i.hover(() =>
-		{
-			$i.css("borderColor", i_color);
-			$i.css("backgroundColor", s_color);
-		}, () =>
-		{
-			$i.css("borderColor", s_color);
-			$i.css("backgroundColor", i_color);
-		});
+		return $i;
 	}
 
 	link(id, URL)
 	{
 		let $i = $(`#${id}`);
-		$i.before($(`<a id="link_${id}" href="${URL}"></a>`));
-		$i.appendTo($(`#link_${id}`));
+		$i.before($(`<a id="link_${id}" href="${URL}"></a>`)).appendTo($(`#link_${id}`));
+
+		return $i;
 	}
 
 	text_in_circle(circle, id, text, size, o_color, c_color, use_ExMD, ExMD)
 	{
+		if (is_empty(id)) id = `FS_default_${this.default_id.text++}`;
+
 		let $c = $(`#circle_${circle}`);
 		$c.append($(`<p id="${id}" class="text">${text}</p>`));
 		let $i = $(`#${id}`);
-		$i.css("fontSize", `${size}px`);
-		$i.css("color", o_color);
-		$i.parent().hover(() =>
-		{
-			$i.css("color", c_color);
-		}, () =>
-		{
-			$i.css("color", o_color);
-		});
+		$i
+		.css("fontSize", `${size}px`)
+		.css("color", o_color)
+		.parent().hover(() => $i.css("color", c_color),
+		                () => $i.css("color", o_color));
+
 		if (use_ExMD)
 		{
-			if (typeof(ExMD.render) !== "function") this.error_and_throw("R002", "Require ExtendedMarkdownParser but didn't get a correct ExtendedMarkdownParser object.");
+			if (!ExMD instanceof ExtendedMarkdownParser) this.error_and_throw("R002", "Require ExtendedMarkdownParser but didn't get a correct ExtendedMarkdownParser object.");
 			ExMD.render($i, $i.html());
 		}
+
+		return $i;
 	}
 
 	layer(id, layer)
