f43252c5ab0918d22f05b65cd1b183ce6f86c2eb
ForkΨKILLET <fork_killet@qq.com>
2023-03-08 18:31:40 +0800

    fix(main): title overflow

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { useRoute } from 'vue-router';
 
 const route = useRoute()
+const routeId = computed(() => route.params.id)
 </script>
 
 <template>
     <div class="home">
         <h1>{{ route.name }}
-            <small v-if="route.params.id">/ {{ route.params.id }}</small>
+            <small v-if="routeId">/ {{ routeId }}</small>
         </h1>
         <main>
             <RouterView></RouterView>
@@ -34,8 +36,11 @@ const route = useRoute()
 h1 {
     height: 1em;
     margin: 0 0 1em 0;
+    padding: .2em 0;
 
+    white-space: nowrap;
     overflow: hidden;
+    text-overflow: clip;
 }
 h1 > small {
     color: #888;
