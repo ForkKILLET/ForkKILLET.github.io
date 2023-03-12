b4d307328ed38e78d2fbc0db3dcb4d34fc5f1bda
ForkΨKILLET <fork_killet@qq.com>
2023-03-12 17:17:00 +0800

    feat(main): add entering animation

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
 const routeId = computed(() => route.params.id)
+
+const entering = ref(false)
 </script>
 
 <template>
@@ -11,8 +13,16 @@ const routeId = computed(() => route.params.id)
         <h1>{{ route.name }}
             <small v-if="routeId">/ {{ routeId }}</small>
         </h1>
-        <main>
-            <RouterView></RouterView>
+        <main :class="{ entering }">
+            <RouterView v-slot="{ Component }">
+                <Transition
+                    name="float"
+                    @before-enter="entering = true"
+                    @after-enter="entering = false"
+                >
+                    <component :is="Component"></component>
+                </Transition>
+            </RouterView>
         </main>
     </div>
 </template>
@@ -30,6 +40,19 @@ const routeId = computed(() => route.params.id)
 }
 .home {
     padding: 1.5em;
+}
+
+.float-enter-active {
+    overflow-x: hidden;
+    transition: .5s transform, .5s opacity;
+}
+.float-enter-from {
+    opacity: 0;
+    transform: translateX(5vw);
+}
+.float-enter-to {
+    opacity: 1;
+    transform: none;
 }
 
 h1 {
@@ -51,8 +74,12 @@ main {
     padding: 1.5em;
 
     overflow: auto;
+    overflow-anchor: none;
     
     background-color: white;
     box-shadow: 0 0 .5em #aaa;
+}
+main.entering {
+    overflow-x: hidden;
 }
 </style>
