a1500cbf9757d82dcdc051bafea413cf0ee20700
ForkΨKILLET <fork_killet@qq.com>
2023-04-07 12:39:02 +0800

    build: split vendor

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import prismjs from 'vite-plugin-prismjs'
 
 export default defineConfig({
     plugins: [
+        splitVendorChunkPlugin(),
         vue(),
         prismjs({
             languages: [ 'javascript', 'typescript', 'bash', 'markup' ],
